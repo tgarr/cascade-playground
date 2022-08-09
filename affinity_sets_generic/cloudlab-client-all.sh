e7b01d4f06b2470d093fa9e318ea7076d365f519
Thiago Garrett <thgarrett@gmail.com>
2022-08-09 14:54:12 -0400

    small fix

--- affinity_sets_generic/cloudlab-client-all.sh
+++ affinity_sets_generic/cloudlab-client-all.sh
@@ -7,13 +7,13 @@ num_entry_parts="1"
 data_part_size="1000000"
 num_data_parts="10 20 30 40 50"
 
-for epsz in $data_part_size; do
-    for nep in $num_data_parts; do
+for epsz in $entry_part_size; do
+    for nep in $num_entry_parts; do
         for dpsz in $data_part_size; do
             for ndp in $num_data_parts; do
                 for osz in $object_size; do
                     for orate in $object_rate; do
-                        if ! grep -q " $osz $orate $dpsz $ndp " client.results; then
+                        if ! grep -q " $osz $orate $epsz $nep $dpsz $ndp " client.results; then
                             ./cloudlab-client.sh $osz $orate $epsz $nep $dpsz $ndp
                             sleep 2
                         fi
