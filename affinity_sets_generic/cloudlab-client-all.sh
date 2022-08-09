0bcc30dd76835269a0213b4ba2e388ecf91aa95d
Thiago Garrett <thgarrett@gmail.com>
2022-08-09 16:51:59 -0400

    tweak to client script

--- affinity_sets_generic/cloudlab-client-all.sh
+++ affinity_sets_generic/cloudlab-client-all.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 object_size="1000"
-object_rate="10 20 30 40 50"
 entry_part_size="1000000"
-num_entry_parts="1"
 data_part_size="1000000"
+
+object_rate="10 20 30 40 50 60 70 80 90 100"
+num_entry_parts="1 2 4 6 8 10"
 num_data_parts="10 20 30 40 50"
 
 for epsz in $entry_part_size; do
@@ -15,7 +16,8 @@ for epsz in $entry_part_size; do
                     for orate in $object_rate; do
                         if ! grep -q " $osz $orate $epsz $nep $dpsz $ndp " client.results; then
                             ./cloudlab-client.sh $osz $orate $epsz $nep $dpsz $ndp
-                            sleep 2
+                            echo -n "Press enter to continue ..."; read
+                            sleep 1
                         fi
                     done
                 done
