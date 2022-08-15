0f484359d1f50a40a7c29c438d70c2aa66bc65ce
Thiago Garrett <thgarrett@gmail.com>
2022-08-15 19:07:54 -0400

    waiting less

--- affinity_sets_generic/common.hpp
+++ affinity_sets_generic/common.hpp
@@ -40,9 +40,9 @@ using namespace derecho::cascade;
 #define ENTRY_AFFINITY_KEY "entry"
 
 #define CLIENT_RETURN_UDP_PORT 43259
-#define CLIENT_RETURN_TIMEOUT 10
+#define CLIENT_RETURN_TIMEOUT 5
 #define RETURN_MESSAGE_SIZE sizeof(int)*3
-#define CLIENT_WAIT_BEFORE_END 30
+#define CLIENT_WAIT_BEFORE_END 10
 
 #define CLIENT_SEED 49357
 #define RANDOM_BUFFER_CHUNK 100
