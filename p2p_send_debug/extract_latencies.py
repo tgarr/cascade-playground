c51026b6a681686fc26ac9470dc88acd25ab6b4a
Thiago Garrett <thgarrett@gmail.com>
2022-08-18 15:55:25 -0400

    oops

--- p2p_send_debug/extract_latencies.py
+++ p2p_send_debug/extract_latencies.py
@@ -26,16 +26,17 @@ for line in f.readlines():
         except:
             pass
 
+count = count/2
 p2p_array = np.array(p2p_latencies)
 wait_array = np.array(wait_latencies)
 
-print("P2P_SEND (%d samples):" % count/2)
+print("P2P_SEND (%d samples):" % count)
 print("  avg: %.2f" % np.mean(p2p_array))
 print("  std: %.2f" % np.std(p2p_array))
 print("  min: %d" % np.min(p2p_array))
 print("  max: %d" % np.max(p2p_array))
 
-print("WAIT (%d samples):" % count/2)
+print("WAIT (%d samples):" % count)
 print("  avg: %.2f" % np.mean(wait_array))
 print("  std: %.2f" % np.std(wait_array))
 print("  min: %d" % np.min(wait_array))
