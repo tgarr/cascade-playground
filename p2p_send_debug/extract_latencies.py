403bbfe21de1bf983dcee5f10620fccb1d1408c2
Thiago Garrett <thgarrett@gmail.com>
2022-08-18 15:52:38 -0400

    tweak

--- p2p_send_debug/extract_latencies.py
+++ p2p_send_debug/extract_latencies.py
@@ -6,6 +6,7 @@ OBJ_SKIP = 500
 
 f = open(sys.argv[1],"r")
 
+count = 0
 p2p_latencies = []
 wait_latencies = []
 
@@ -21,19 +22,20 @@ for line in f.readlines():
         try:
             if values[0] == "P2P_SEND": p2p_latencies.append(int(v))
             elif values[0] == "WAIT": wait_latencies.append(int(v))
+            count += 1
         except:
             pass
 
 p2p_array = np.array(p2p_latencies)
 wait_array = np.array(wait_latencies)
 
-print("P2P_SEND:")
+print("P2P_SEND (%d samples):" % count)
 print("  avg: %.2f" % np.mean(p2p_array))
 print("  std: %.2f" % np.std(p2p_array))
 print("  min: %d" % np.min(p2p_array))
 print("  max: %d" % np.max(p2p_array))
 
-print("WAIT:")
+print("WAIT (%d samples):" % count)
 print("  avg: %.2f" % np.mean(wait_array))
 print("  std: %.2f" % np.std(wait_array))
 print("  min: %d" % np.min(wait_array))
