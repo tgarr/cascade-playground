a6ed86672d3c8a2a9f040658de17d9d10f0a944d
Thiago Garrett <thgarrett@gmail.com>
2022-08-30 14:26:33 -0400

    script

--- udl_microbenchmark/get_latencies.py
+++ udl_microbenchmark/get_latencies.py
@@ -2,6 +2,8 @@
 import sys
 import numpy as np
 
+SKIP = 100
+
 TAG_UDL_GET_START = 8001
 TAG_UDL_GET_END = 8002
 TAG_UDL_WAIT_END = 8003
@@ -24,16 +26,18 @@ if __name__ == "__main__":
             if tag in timestamps:
                 timestamps[tag].append(value)
 
-    udl_get_start = np.array(timestamps[TAG_UDL_GET_START])/1e3
-    udl_get_end = np.array(timestamps[TAG_UDL_GET_END])/1e3
-    udl_wait_end = np.array(timestamps[TAG_UDL_WAIT_END])/1e3
+    udl_get_start = np.array(timestamps[TAG_UDL_GET_START][SKIP:])/1e3
+    udl_get_end = np.array(timestamps[TAG_UDL_GET_END][SKIP:])/1e3
+    udl_wait_end = np.array(timestamps[TAG_UDL_WAIT_END][SKIP:])/1e3
 
     udl_get_lat = udl_get_end - udl_get_start
     udl_wait_lat = udl_wait_end - udl_get_end
-    throughput = len(udl_get_lat) / float(udl_wait_end[-1] - udl_get_start[0]) * 1000000
+    udl_complete_lat = udl_wait_end[-1] - udl_get_start[0]
+    throughput = len(udl_get_lat) / float(udl_complete_lat) * 1000000
 
     #print(f"num samples: {len(udl_get_lat)}")
     print(f"udl_get_lat: {np.mean(udl_get_lat):.2f} / {np.std(udl_get_lat):.2f}")
     print(f"udl_wait_lat: {np.mean(udl_wait_lat):.2f} / {np.std(udl_wait_lat):.2f}")
+    print(f"udl_complete_lat: {udl_complete_lat}")
     print(f"throughput: {throughput:.2f}")
 
