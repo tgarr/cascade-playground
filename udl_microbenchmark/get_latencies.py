c686c89f63ecc5547acdf2a8a19229dfeeec80f9
Thiago Garrett <thgarrett@gmail.com>
2022-08-30 17:21:32 -0400

    fix

--- udl_microbenchmark/get_latencies.py
+++ udl_microbenchmark/get_latencies.py
@@ -1,8 +1,10 @@
 
 import sys
 import numpy as np
+import array
 
 SKIP = 100
+TOTAL = 1100
 
 TAG_UDL_GET_START = 8001
 TAG_UDL_GET_END = 8002
@@ -10,25 +12,32 @@ TAG_UDL_WAIT_END = 8003
 
 if __name__ == "__main__":
     fname = sys.argv[1]
+    if len(argv) > 2:
+        total = int(argv[2]) - SKIP
+    else:
+        total = TOTAL - SKIP
 
     timestamps = dict({
-        TAG_UDL_GET_START: [],
-        TAG_UDL_GET_END: [],
-        TAG_UDL_WAIT_END: [],
+        TAG_UDL_GET_START: array.array('q',[0]*total),
+        TAG_UDL_GET_END: array.array('q',[0]*total),
+        TAG_UDL_WAIT_END: array.array('q',[0]*total),
         })
     
     with open(fname,"r") as f:
         for line in f:
             fields = line.split()
             tag = int(fields[0])
+            obj_id = int(fields[2])
             value = int(fields[3])
 
-            if tag in timestamps:
-                timestamps[tag].append(value)
+            if obj_id < SKIP: continue
 
-    udl_get_start = np.array(timestamps[TAG_UDL_GET_START][SKIP:])/1e3
-    udl_get_end = np.array(timestamps[TAG_UDL_GET_END][SKIP:])/1e3
-    udl_wait_end = np.array(timestamps[TAG_UDL_WAIT_END][SKIP:])/1e3
+            if tag in timestamps:
+                timestamps[tag][obj_id-SKIP] = value
+
+    udl_get_start = np.array(timestamps[TAG_UDL_GET_START])/1e3
+    udl_get_end = np.array(timestamps[TAG_UDL_GET_END])/1e3
+    udl_wait_end = np.array(timestamps[TAG_UDL_WAIT_END])/1e3
 
     udl_get_lat = udl_get_end - udl_get_start
     udl_wait_lat = udl_wait_end - udl_get_end
